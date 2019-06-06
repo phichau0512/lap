0c0ad023f147ccff8ec280e466765d67686c51a3
phichau0512 <you@example.com>
2019-06-06 14:49:29 +0700

    Create mang1chieu.java

---
+++ mang1chieu.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+
+
+public class mang1chieu {
+public static void main(String[] args) {
+//	int a;
+//	Scanner ghi=new Scanner(System.in);
+//	System.out.println("nhap mang:");
+//	a=ghi.nextInt();
+//	int[] mang= new int [a];
+//	for (int i = 0; i < a; i++) {
+//        System.out.println("Nhập vào phần tử thứ " + i + ": ");
+//        mang[i] = ghi.nextInt();
+//	}
+//	for (int i = 0; i < a; i++) {
+//        System.out.println("Phần tử thứ " + i + ": " + mang[i]);
+//    }
+	
+	
+	//tinh tbc
+
+
+
+	    int A, tong=0 , so;
+	    float tbc;
+	    Scanner ghi = new Scanner(System.in);
+	    System.out.println("Nhập vào số phần tử của mảng: ");
+	    A = ghi.nextInt();
+	    int[] mang = new int[A];
+	    
+	
+
+	    tbc = (float)tong/A;
+	    System.out.println("Trung Bình Cộng: " + tbc);
+	    }
+
+}
